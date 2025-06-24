4ae02493a22eee4d24ea5e2d9f41f3eab6f39f51
kristijanabacus <kristijan@abacusplus.ba>
2025-06-24 15:59:13 +0200

    newsitedeploy

--- abacusplus/blocks/before-after/admin.php
+++
@@ -1,5 +0,0 @@
-<div class="ap-block-default-preview">
-    <div class="ap-block-default-preview-title">
-        <p>Before After Block</p>
-    </div>
-</div>
--- abacusplus/blocks/palette/admin.php
+++
@@ -1,5 +0,0 @@
-<div class="ap-block-default-preview">
-    <div class="ap-block-default-preview-title">
-        <p>Palette Block</p>
-    </div>
-</div>
